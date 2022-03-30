25686b64ee87236ef0a1af89bbaaac38d49a69e7
Huy Pham <huypq104@gmail.com>
2022-03-30 19:28:26 +0700

    chore: update deps, adapt tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,68 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable quotes */
+const plugin = require("tailwindcss/plugin");
+
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                body: [
+                    '"Open Sans"',
+                    "ui-sans-serif",
+                    "system-ui",
+                    "-apple-system",
+                    "BlinkMacSystemFont",
+                    '"Segoe UI"',
+                    "Roboto",
+                    '"Helvetica Neue"',
+                    "Arial",
+                    '"Noto Sans"',
+                    "sans-serif",
+                    '"Apple Color Emoji"',
+                    '"Segoe UI Emoji"',
+                    '"Segoe UI Symbol"',
+                    '"Noto Color Emoji"',
+                ],
+            },
+            spacing: {
+                header: "4rem",
+                icon: "24px",
+                "icon-sm": "1rem",
+                "icon-xs": "11px",
+                input: "2.5rem",
+                "input-md": "2.75rem",
+                "input-lg": "3rem",
+                sidebar: "18rem",
+                modal: "480px",
+            },
+        },
+    },
+    plugins: [
+        require("@tailwindcss/line-clamp"),
+        plugin(({ addUtilities }) => {
+            addUtilities({
+                ".animate-pause": {
+                    "animation-play-state": "paused",
+                },
+                ".scrollbar-none": {
+                    "scrollbar-width": "none",
+                    "&::-webkit-scrollbar": {
+                        display: "none",
+                    },
+                },
+                ".scrollbar-thin": {
+                    "scrollbar-width": "thin",
+                    "&::-webkit-scrollbar": {
+                        width: "5px",
+                        height: "8px",
+                        background: "#dddddd",
+                    },
+                    "&::-webkit-scrollbar-thumb": {
+                        background: "#777777",
+                    },
+                },
+            });
+        }),
+    ],
+};
