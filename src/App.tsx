8aee2ebbea174f3444df10d883ceb8c9c6fd4660
Sotatek-HuyPham2 <huy.pham2@sotatek.com>
2022-03-30 22:10:03 +0700

    chore: scaff routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback, useEffect, useRef, useState } from "react";
 import AppContext, { TAppContext } from "./contexts/AppContext";
+import { Routes, Route, Link } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./app/store";
 
@@ -33,9 +34,32 @@ function App(): React.ReactElement {
             <div className="App">
                 <header className="App-header">
                     <h1 className="text-xl">LEGEND-DAO</h1>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="about" element={<About />} />
+                    </Routes>
                 </header>
             </div>
         </AppContext.Provider>
+    );
+}
+
+function Home(): React.ReactElement {
+    return (
+        <div>
+            <h1>LEGEND-DAO#HOME</h1>
+            <header>
+                <nav>
+                    <Link to="/about">About</Link>
+                </nav>
+            </header>
+        </div>
+    );
+}
+
+function About(): React.ReactElement {
+    return (
+        <div>LEGEND-DAO#ABOUT</div>
     );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+/**
+ * To future if Legend-DAO need a routing:
+ * Serving Apps with Client-Side Routing
+ * https://create-react-app.dev/docs/deployment/#serving-apps-with-client-side-routing
+ */
+
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </React.StrictMode>,
     document.getElementById("root")
 );
