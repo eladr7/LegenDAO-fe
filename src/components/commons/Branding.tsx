bd0680973d9db3cecd3890177a8c4ee491b904a1
Sotatek-HuyPham2 <huy.pham2@sotatek.com>
2022-04-20 07:07:26 +0700

    fix: LD-49

--- src/components/commons/Branding.tsx
+++ src/components/commons/Branding.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export function Branding(): React.ReactElement {
     return (
         <div className="flex flex-col flex-nowrap text-white">
-            <Link to="/">
+            <Link to="/mint-lab">
                 <div className="font-bold leading-none">The Legendao</div>
                 <div className="text-2xl leading-none font-bold">Universe</div>
             </Link>
