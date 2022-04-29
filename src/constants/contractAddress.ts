13efc4cef94e32f98f491a33fa383a7c56355ddd
Sotatek-TuanNguyen5 <tuan.nguyen5@sotatek.com>
2022-04-29 17:46:57 +0700

    update smart contract address

--- src/constants/contractAddress.ts
+++ src/constants/contractAddress.ts
@@ -2,3 +2,4 @@ export const LGND_ADDRESS = process.env.REACT_APP_ADDRESS_LGND;
 export const PLATFORM_ADDRESS = process.env.REACT_APP_ADDRESS_PLATFORM;
 export const NFT_ADDRESS = process.env.REACT_APP_ADDRESS_NFT;
 export const NFT_MINTING_ADDRESS = process.env.REACT_APP_ADDRESS_NFT_MINTING;
+export const STAKING_ADDRESS = process.env.REACT_APP_ADDRESS_STAKING;
