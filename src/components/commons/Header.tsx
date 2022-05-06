7f8fa40c9d95e40320f2f25f4597c9894e6b5694
Sotatek-HuyPham2 <huy.pham2@sotatek.com>
2022-05-06 15:17:39 +0700

    fix: popup queue

--- src/components/commons/Header.tsx
+++ src/components/commons/Header.tsx
@@ -56,6 +56,7 @@ export function Header({
             dispatch(toggleDepositPanel(false));
             return;
         }
+        dispatch(accessibilityActions.toggleCreationFormPanel(false));
         dispatch(toggleBalanceMenu());
     }, [accessibilityState.bDepositPanelOn, accessibilityState.bWithdrawPanelOn, dispatch]);
 
