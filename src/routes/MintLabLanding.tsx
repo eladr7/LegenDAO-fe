31f4baad4a4906619033c31b2cee44acae48b2db
Nothing <nothing@Nothings-iMac.local>
2022-05-10 16:23:08 +0700

    fix try connect

--- src/routes/MintLabLanding.tsx
+++ src/routes/MintLabLanding.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from "react";
-import { useAppDispatch } from "../app/hooks";
+import React from "react";
 import { DefaultLayout } from "../components/layouts/DefaultLayout";
 import LegendaryPlatformArticle from "../components/LegendaryPlatformArticle";
 import LegendaryTokenArticle from "../components/LegendaryTokenArticle";
@@ -8,14 +7,8 @@ import MountainsOfSodArticle from "../components/MountainsOfSodArticle";
 import RoadmapArticle from "../components/RoadmapArticle";
 import SecretCollectionsArticle from "../components/SecretCollectionsArticle";
 import TeamArticle from "../components/TeamArticle";
-import { networkActions } from "../features/network/networkSlice";
 
 export function MintLabLanding(): React.ReactElement {
-    const dispatch = useAppDispatch();
-
-    useEffect(() => {
-        dispatch(networkActions.tryConnecting());
-    }, [dispatch]);
 
     return (
         <DefaultLayout headerType="general" sidebarTab="tab/home" bFooterOn bHeaderAlwaysOnTop>
