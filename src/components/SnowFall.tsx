6cf30c3bd45a14a191159be79fac66c138b4a3a2
Sotatek-HuyPham2 <huy.pham2@sotatek.com>
2022-05-16 17:19:43 +0700

    chore: smaller snow

--- src/components/SnowFall.tsx
+++ src/components/SnowFall.tsx
@@ -8,7 +8,7 @@ export default function SnowFall(): React.ReactElement {
                 <div className="animate-snowfall-slow-r-l snow-flower -top-[10vh] left-[10vw] odd:w-1/2 odd:h-1/2 even:w-2/3 even:h-2/3 bg-white rounded-full absolute"></div>
                 <div className="animate-snowfall-normal-r-l -top-[10vh] odd:w-1/2 odd:h-1/2 even:w-2/3 even:h-2/3 bg-white rounded-full absolute"></div>
             </div>
-            <div className="snow-group relative w-4 h-4 grow-0 shrink-0 overflow-visible">
+            <div className="snow-group relative w-2 h-2 grow-0 shrink-0 overflow-visible">
                 <div className="animate-snowfall-normal-l-r snow-flower -top-[10vh] odd:w-1/2 odd:h-1/2 even:w-2/3 even:h-2/3 bg-white rounded-full absolute"></div>
                 <div className="animate-snowfall-normal-r-l snow-flower -top-[10vh] left-[10vw] odd:w-1/2 odd:h-1/2 even:w-2/3 even:h-2/3 bg-white rounded-full absolute"></div>
                 <div className="animate-snowfall-slow-r-l -top-[10vh] odd:w-1/2 odd:h-1/2 even:w-2/3 even:h-2/3 bg-white rounded-full absolute"></div>
@@ -23,12 +23,12 @@ export default function SnowFall(): React.ReactElement {
                 <div className="animate-snowfall-fast-r-l snow-flower -top-[10vh] left-[10vw] odd:w-1/2 odd:h-1/2 even:w-2/3 even:h-2/3 bg-white rounded-full absolute"></div>
                 <div className="animate-snowfall-slow-r-l -top-[10vh] odd:w-1/2 odd:h-1/2 even:w-2/3 even:h-2/3 bg-white rounded-full absolute"></div>
             </div>
-            <div className="snow-group relative w-4 h-4 grow-0 shrink-0 overflow-visible">
+            <div className="snow-group relative w-3 h-3 grow-0 shrink-0 overflow-visible">
                 <div className="animate-snowfall-fast-l-r snow-flower -top-[10vh] odd:w-1/2 odd:h-1/2 even:w-2/3 even:h-2/3 bg-white rounded-full absolute"></div>
                 <div className="animate-snowfall-slow-r-l snow-flower -top-[10vh] left-[10vw] odd:w-1/2 odd:h-1/2 even:w-2/3 even:h-2/3 bg-white rounded-full absolute"></div>
                 <div className="animate-snowfall-normal-r-l -top-[10vh] odd:w-1/2 odd:h-1/2 even:w-2/3 even:h-2/3 bg-white rounded-full absolute"></div>
             </div>
-            <div className="snow-group relative w-4 h-4 grow-0 shrink-0 overflow-visible">
+            <div className="snow-group relative w-2 h-2 grow-0 shrink-0 overflow-visible">
                 <div className="animate-snowfall-normal-l-r snow-flower -top-[10vh] odd:w-1/2 odd:h-1/2 even:w-2/3 even:h-2/3 bg-white rounded-full absolute"></div>
                 <div className="animate-snowfall-normal-r-l snow-flower -top-[10vh] left-[10vw] odd:w-1/2 odd:h-1/2 even:w-2/3 even:h-2/3 bg-white rounded-full absolute"></div>
                 <div className="animate-snowfall-slow-r-l -top-[10vh] odd:w-1/2 odd:h-1/2 even:w-2/3 even:h-2/3 bg-white rounded-full absolute"></div>
